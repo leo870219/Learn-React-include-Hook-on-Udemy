674949c2dffe25c14be1aac48a9a2014cc73aa22
R_XIAN <asdzxccxz333@gmail.com>
2020-04-04 17:31:17 +0800

    now set

--- src/commons/axios.js
+++ src/commons/axios.js
@@ -1,18 +1,19 @@
 import _axios from "axios";
 const axios = (baseURL) => {
   const instance = _axios.create({
-    baseURL: baseURL || "http://localhost:3003/",
+    baseURL:
+      baseURL || process.env.REACT_APP_API_DOMIN || "http://localhost:3003/",
     timeout: 1000,
   });
 
   instance.interceptors.request.use(
-    config => {
+    (config) => {
       const jwToken = global.auth.getToken();
-      config.headers['Authorization'] = 'Bearer ' + jwToken;
+      config.headers["Authorization"] = "Bearer " + jwToken;
       // Do something before request is sent
       return config;
     },
-    error => {
+    (error) => {
       // Do something with request error
       return Promise.reject(error);
     }
